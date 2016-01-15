4eedf7f14bf96fe6e3df5dbe269f3cd9b008088c
gracie lundell and haley bristol <gracielundell@gmail.com>
2016-01-14 16:07:02 -0800

    Add dropdown answer bars git push gracie master

--- js/scripts.js
+++ js/scripts.js
@@ -1,2 +1,28 @@
 $(document).ready(function(){
-  $("form#dating").submit(function(event) {
+  $("#question1").click(function(event) {
+    var 
+  });
+
+
+//   submit(function(event) {
+//   var num1 = parseInt($("button#1").val());
+//   var num2 = $("button#2").val();
+//   var num3 = $("button#3").val();
+//   var num4 = $("button#4").val();
+//   var num5 = $("button#5").val();
+//
+//
+//
+//
+//   $("form#dating").submit(function(event) {
+//   var num1 = parseInt($("button#1").val());
+//   var num2 = $("button#2").val();
+//   var num3 = $("button#3").val();
+//   var num4 = $("button#4").val();
+//   var num5 = $("button#5").val();
+//
+//   num1 === int(1)
+//
+// var baby = $()
+
+});
