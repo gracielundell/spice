ffdc663c19d9fdd74004374c6b01bdfce2eca16e
Gracie Lundell <gracielundell@gmail.com>
2016-01-14 21:52:47 -0800

    make jQuery work for different options

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,29 @@
 $(document).ready(function(){
-  $("#sell1").change(function() {
-    var value1 = parseInt($("select#sell1").val());
-    console.log(value1);
-  });
+  $("form").submit(function(event) {
+    var value = parseInt($("select#sell1").val());
+    var total = value;
+    console.log("value = ", value);
 
-   if 2 === 
-});
+     if (total === 1) {
+       $("#spicegirl").remove();
+      $(".baby").show();
+      }
+      else if (total === 2) {
+        $("#spicegirl").remove();
+        $(".sporty").show();
+      }
+      else if (total === 3) {
+        $("#spicegirl").remove();
+        $(".scary").show();
+      }
+      else if (total === 4) {
+        $("#spicegirl").remove();
+        $(".posh").show();
+      }
+      else if (total === 5) {
+        $("#spicegirl").remove();
+        $(".ginger").show();
+      }
+      event.preventDefault();
+    });
+  });
