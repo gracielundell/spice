eaa8269cf4f7edccc55f3a40bba8d2ecb62b8da6
gracie lundell and haley bristol <gracielundell@gmail.com>
2016-01-14 16:26:36 -0800

    Add working jquery

--- js/scripts.js
+++ js/scripts.js
@@ -1,28 +1,8 @@
 $(document).ready(function(){
-  $("#question1").click(function(event) {
-    var 
+  $("#sell1").change(function() {
+    var value1 = parseInt($("select#sell1").val());
+    console.log(value1);
   });
 
-
-//   submit(function(event) {
-//   var num1 = parseInt($("button#1").val());
-//   var num2 = $("button#2").val();
-//   var num3 = $("button#3").val();
-//   var num4 = $("button#4").val();
-//   var num5 = $("button#5").val();
-//
-//
-//
-//
-//   $("form#dating").submit(function(event) {
-//   var num1 = parseInt($("button#1").val());
-//   var num2 = $("button#2").val();
-//   var num3 = $("button#3").val();
-//   var num4 = $("button#4").val();
-//   var num5 = $("button#5").val();
-//
-//   num1 === int(1)
-//
-// var baby = $()
-
+  // if 2 === baby pic etc
 });
